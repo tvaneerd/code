a55966a97ce8ba5ff5becddeb2d91c5746191bf5
Tony Van Eerd <tvaneerd@gmail.com>
2018-07-05 18:19:29 -0400

    rename to stable_sample()
    Thanks to Seph

--- sampling.h
+++ sampling.h
@@ -1,14 +1,16 @@
+#ifndef sampling_h_INCLUDED
+#define sampling_h_INCLUDED
+
 #include <iterator> // std::distance
 #include <random> // uniform_int_distribution
 #include <algorithm> // for_each (is it worth an include just for that?)
 
 // like "Reservoir sampling", without the reservoir
-// because we want an out() function, and thus don't have a fixed place to temporarily put things.
-// (out() allows you to transform as you sample)
-//
+// because we want an out() function, not a fixed place to put things.
 // similar to std::sample() coming in C++17
+// Note that this maintains the order of the selected samples, thus "stable"_sample
 template <typename Iterator, typename Sentinel, typename UniformRandomNumberGenerator, typename Output>
-void sample(Iterator begin, Sentinel end, int sampleSize, UniformRandomNumberGenerator& urng, Output & out)
+void stable_sample(Iterator begin, Sentinel end, int sampleSize, UniformRandomNumberGenerator& urng, Output & out)
 {
     auto left = std::distance(begin, end); // how many left to choose from
     using DistType = decltype(left);
@@ -29,16 +31,16 @@ void sample(Iterator begin, Sentinel end, int sampleSize, UniformRandomNumberGen
     //  ie second item's total odds of failing are 10/11 * 1/10 = 1/11.  Oh, would you look at that.)
     // Do the math, or google it, it works out.
 
-    for (Iterator curr = begin;  need != 0;  curr++) // curr is just a better name than begin, seems weird to incr begin
+    for (Iterator curr = begin; need > 0; curr++) // curr is just a better name than begin
     {
         if (left <= need) {
             // everyone left "wins", because we still need at least that many
-            // (note: we only fail to get the full sampleSize if we started with less than enough items)
+            // (note: we only fail to get the full sampleSize if we started with less than that many items)
             std::for_each(curr, end, out);
             return;
         }
         left--; // 0 to 99, not 1 to 100, because C++; and next time through it will be 0 to 98, etc
-        // select # from 0 to 99:
+                // select # from 0 to 99:
         DistType r = std::uniform_int_distribution<DistType>(0, left)(urng);
         // if # is less than 10 (or whatever still needed), you are one of the 10 for the sample!
         if (r < need) {
@@ -50,16 +52,18 @@ void sample(Iterator begin, Sentinel end, int sampleSize, UniformRandomNumberGen
 }
 
 template<typename T, typename Transform>
-std::vector<T> sample(std::vector<T> const & in, int count, Transform const & transform)
+std::vector<T> sample(std::vector<T> const & vin, int count, Transform const & transform)
 {
     std::vector<T> out;
-    if (in.size() < count)
-        out = in;
+    if (vin.size() < count)
+        out = vin;
     else {
         std::random_device rd;
         std::mt19937 gen(rd());
         out.reserve(count);
-        sample(in.begin(), in.end(), count, gen, [&transform, &out](T const & elem) { out.push_back(transform(elem)); });
+        stable_sample(vin.begin(), vin.end(), count, gen, [&transform, &out](T const & elem) { out.push_back(transform(elem)); });
     }
     return out;
 }
+
+#endif // _h
